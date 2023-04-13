518e674f64be6cd8142f7df02411c148d390d916
Bobby Cannon <bobby.cannon@live.com>
2023-04-13 10:10:05 -0400

    aoeu

---
+++ MapControl/MapControlTest/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MapControlTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+	        Map.Center = new Geopoint(
+		        new BasicGeoposition
+		        {
+			        Altitude = 0,
+			        Latitude = -37.8155,
+			        Longitude = 144.965
+		        }, AltitudeReferenceSystem.Terrain
+	        );
+	        Map.ZoomLevel = 16;
+        }
+    }
+}
